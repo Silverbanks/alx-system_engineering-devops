882c37b0f62ce7c226502b903cf477beac769c13
Silverbanks <118997033+Silverbanks@users.noreply.github.com>
2023-08-11 02:46:34 +0100

    update

---
+++ 0x17-web_stack_debugging_3/0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+# Puppet manifest to fix a bug in wp-setings.php
+
+exec { '/var/www/html/wp-setting.php':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
